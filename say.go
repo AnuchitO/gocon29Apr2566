4ca0100af0e0bffc810db948e8a5c79b940adfa2
Nong AnuchitO <anuchitp@thoughtworks.com>
2023-04-29 10:51:21 +0700

    waitgroup

--- say.go
+++ say.go
@@ -2,18 +2,23 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-func say(s string) {
+func say(s string, wg *sync.WaitGroup) {
 	fmt.Println("say:", s)
 	time.Sleep(1 * time.Second)
+	wg.Done()
 }
 
 func main() {
 	t := time.Now()
 
-	go say("Hello")
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go say("Hello", &wg)
+	wg.Wait()
 
 	fmt.Println("time:", time.Since(t))
 }
