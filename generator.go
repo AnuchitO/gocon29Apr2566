d1fb6386ecc1280cdefef19f2f6f45f0923a097a
Nong AnuchitO <anuchitp@thoughtworks.com>
2023-04-29 13:53:28 +0700

    generator

--- generator.go
+++ generator.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
+func say(msg string) <-chan string {
+	c := make(chan string)
+	go func() {
+		for i := 0; ; i++ {
+			c <- fmt.Sprintf("%s %d", msg, i)
+			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+		}
+	}()
+
+	return c
+}
+
+func main() {
+	gopher := say("gopher")
+	panda := say("panda")
+
+	for {
+		select {
+		case msg := <-gopher:
+			fmt.Println(msg)
+		case msg := <-panda:
+			fmt.Println(msg)
+		}
+	}
 }
